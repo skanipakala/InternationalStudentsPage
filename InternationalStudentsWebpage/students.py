587a14ef517d98936af92ce101579cfcaea0c359
Robert <rchen129@umd.edu>
2023-03-13 12:02:30 -0400

    move students into new file

---
+++ InternationalStudentsWebpage/students.py
@@ -0,0 +1,86 @@
+students = [    
+    {    
+    "id": 1,    
+    "name": "Divyansh Agrawal",        
+    "country": "India",        
+    "picture": "john_doe.jpg",        
+    "title": "Mental health stigma in India causes delays in intervention and treatment",        
+    "subtitle": "Some multigenerational families provide guidance for youth while others pass down misconceptions about mental health.",        
+    "article_file": "InternationalStudentsWebpage/static/articles/Divyansh Agrawal/Divyansh_Agrawal.txt",        
+    "quote": "“As you talk to more people, you get aware, and then you can decide what\'s best for you,” Argawal said.",
+    "author": "By Winter Hawk",
+    "audio_file": "IndiaDivyansh.mp3"    
+    },    
+    {
+    "id": 2,
+    "name": "Sophia Anderson",        
+    "country": "Denmark",        
+    "picture": "jane_smith.jpg",        
+    "title": "Adapting to Life in Canada",        
+    "subtitle": "My journey as an international student",        
+    "article_file": "Sofia_Anderson.txt",        
+    "quote": "I'm still trying to figure out how to live with ADHD, but I feel like the depression is not really that big a deal anymore,” Katharina Andersen said. “I don't go into those slumps anymore because I'm learning how to manage it beforehand.",
+    "author": "John Smith",
+    "audio_file": "divyansh_agrawal_interview.mp3"     
+    }, 
+    {
+    "id": 3,
+    "name": "Luisa Barros-Checcucci",        
+    "country": "Belgium",        
+    "picture": "jane_smith.jpg",        
+    "title": "Adapting to Life in Canada",        
+    "subtitle": "My journey as an international student",        
+    "article_file": "Divyansh Agrawal.txt",        
+    "quote": "I was breaking down and he made me feel terrible,” Barros-Checcucci said. “He was just kind of like, 'okay, well what do you want me to do about it' type of thing.",
+    "author": "John Smith",
+    "audio_file": "divyansh_agrawal_interview.mp3"     
+    }, 
+    {
+    "id": 4,
+    "name": "Ubaid Bakhtiar",        
+    "country": "Pakistan",         
+    "picture": "jane_smith.jpg",        
+    "title": "Adapting to Life in Canada",        
+    "subtitle": "My journey as an international student",        
+    "article_file": "Divyansh Agrawal.txt",   
+    "quote": "I dont have a friend from U.S.A,” Bakhtiar said. “People who are in U.S.A, they prefer to have friends with the people that are from U.S.A",
+    "author": "John Smith",
+    "audio_file": "divyansh_agrawal_interview.mp3"     
+    }, 
+    {
+    "id": 5,
+    "name": "Jane Smith",        
+    "country": "Canada",        
+    "picture": "jane_smith.jpg",        
+    "title": "Adapting to Life in Canada",        
+    "subtitle": "My journey as an international student",        
+    "article_file": "Divyansh Agrawal.txt",   
+    "quote": "I am learning so much from this experience!",
+    "author": "John Smith",
+    "audio_file": "divyansh_agrawal_interview.mp3"     
+    }, 
+    {
+    "id": 6,
+    "name": "Jane Smith",        
+    "country": "Canada",        
+    "picture": "jane_smith.jpg",        
+    "title": "Adapting to Life in Canada",        
+    "subtitle": "My journey as an international student",        
+    "article_file": "Divyansh Agrawal.txt",   
+    "quote": "I am learning so much from this experience!",
+    "author": "John Smith",
+    "audio_file": "divyansh_agrawal_interview.mp3"     
+    }, 
+    {
+    "id": 7,
+    "name": "Jane Smith",        
+    "country": "Canada",        
+    "picture": "jane_smith.jpg",        
+    "title": "Adapting to Life in Canada",        
+    "subtitle": "My journey as an international student",        
+    "article_file": "Divyansh Agrawal.txt",   
+    "quote": "I am learning so much from this experience!",
+    "author": "John Smith",
+    "audio_file": "divyansh_agrawal_interview.mp3"     
+    }
+]
